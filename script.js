982eaeb5df7b9291aa93d0641d0e2fddc3d533b7
Alexandra Costache <48081935+c1ba@users.noreply.github.com>
2021-09-09 12:16:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ todoList.appendChild(todoWish);
 }
 
 let suggestToList = ()=> {
-fetch("http://www.boredapi.com/api/activity/")
+fetch("https://www.boredapi.com/api/activity/")
 .then(response =>{ 
     console.log(response);
     return response.json();})
