873f7ac23e2aea2da138898494e7bc4a2142f5bf
Alexandra Costache <48081935+c1ba@users.noreply.github.com>
2021-09-14 10:35:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//Checks if any data has been saved before, if it was saved it would create a to-do item and add it to the list, taking the innerText as variable. 
+//Then, it selects all checkboxes to provide them the checked states saved in the other key from localStorage.
 let loadLocalStorage = ()=>{
     const tdList = localStorage.getItem("todoList");
     if(tdList){
@@ -7,6 +10,7 @@ let loadLocalStorage = ()=>{
             todoWish.className = "wishDiv";
             let todoWords = document.createElement("label"), checkbox = document.createElement("input");
             checkbox.type = "checkbox";
+            checkbox.className = `cb`;
             todoWords.textContent = text;
             
             todoWords.addEventListener("click",()=>{todoWish.remove();});
@@ -14,9 +18,17 @@ let loadLocalStorage = ()=>{
             todoWish.appendChild(todoWords);
             todoList.appendChild(todoWish);
         }
+
+        const checkedboxList = localStorage.getItem("checkboxStates");
+        checkedboxList ? console.log(`Exists`) : console.log(`Error at retrieving the checkbox states: Do not exist`) ;
         let tdListArr = tdList.split(",");
+        let checkboxStatesArr = checkedboxList.split(",");
         for(i of tdListArr){
             addFromLocalStorage(i);
+        }
+        let checkboxes = document.getElementsByClassName(`cb`);
+        for(i = 0; i < checkboxStatesArr.length; i++){
+            checkboxes[i].checked = (checkboxStatesArr[i].toLowerCase() === 'true');
         }
     }
     else{
@@ -33,6 +45,7 @@ let todoWish = document.createElement("div");
 todoWish.className = "wishDiv";
 let todoWords = document.createElement("label"), checkbox = document.createElement("input");
 checkbox.type = "checkbox";
+checkbox.className = `cb`;
 todoWords.textContent = todoInput;
 
 todoWords.addEventListener("click",()=>{todoWish.remove();});
@@ -53,14 +66,18 @@ fetch("https://www.boredapi.com/api/activity/")
 .catch(()=>{console.log(`Error, I could not return. Stage: fetch`)})
 }
 
+//Saves the innerText from the to-do item div and looks for the elements with a classs set to the checkboxes to collect their states
 let saveToLocalStorage = ()=>{
-let wishes = document.getElementsByClassName("wishDiv");
-let tdList = [];
+let wishes = document.getElementsByClassName("wishDiv"), checkers = document.getElementsByClassName(`cb`);
+let tdList = [], checkersList = Array.from(checkers), cl = [];
 for(i of wishes){
     tdList.push(i.innerText);
 }
+for(i of checkersList){
+cl.push(i.checked);
+}
 localStorage.setItem('todoList', tdList);
-localStorage.setItem('checkedToDos', checkedList);
+localStorage.setItem('checkboxStates', cl);
 }
 
 let submitButton = document.getElementById("submit_button");
